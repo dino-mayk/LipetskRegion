2f05af68c58f4d6ee96e3ac197798da0fee717dd
Nikita <nikitosikrasnov@gmail.com>
2022-12-02 22:23:58 +0300

    index.py

--- LipetskRegion/urls.py
+++ LipetskRegion/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
